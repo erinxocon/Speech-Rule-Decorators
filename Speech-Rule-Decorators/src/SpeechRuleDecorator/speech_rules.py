7508251131300449dc0bfc558ba9c8918d8c7f2a
Erin O'Connell <erinocon5@gmail.com>
2016-01-02 01:31:59 -0700

    renamed file decorators.py to speech_rules.py inside of SpeechRuleDecorator module

--- Speech-Rule-Decorators/src/SpeechRuleDecorator/speech_rules.py
+++ Speech-Rule-Decorators/src/SpeechRuleDecorator/speech_rules.py
@@ -1,12 +1,17 @@
-﻿from collections import defaultdict
+﻿"""
+Speech Rule Decorator Module
+Need to add docstring
+"""
+from collections import defaultdict
 import speech_recognition as sr
 
-class SpeechRuleDecorator:
+class SpeechRules:
     """
     Speech Rule Decorator class
     Accepts an optional audio source input from the speech_recognition module
-    """ 
-    def __init__(self, audio_source = sr.Microphone()):
+    """
+
+    def __init__(self, audio_source=sr.Microphone()):
         """Initalizer"""
         self._recognizer = sr.Recognizer()
         self._source = audio_source
@@ -18,33 +23,37 @@ class SpeechRuleDecorator:
 
 
     def _callback(self, recognizer, audio):
-        """Internal callback that looks in the function registry to see if the spoken text is a rule"""
+        """
+        Internal callback that looks in the function registry
+        to see if the spoken text is a rule
+        """
         # received audio data, now we'll recognize it using Google Speech Recognition
         try:
             stt = recognizer.recognize_google(audio)
             try:
-                for func in self._func_registry.get(stt):
+                for func in self._func_registry.get(stt.lower()):
                     func()
 
-            except KeyError as e:
-                print("Rule '{0} not found.'".format(e))
+            except KeyError as error:
+                print("Rule '{0} not found.'".format(error))
 
         except sr.UnknownValueError:
             print("Google Speech Recognition could not understand audio")
 
-        except sr.RequestError as e:
-            print("Could not request results from Google Speech Recognition service; {0}".format(e))
+        except sr.RequestError as error:
+            print("Could not request results from Google Speech Recognition service; {0}".format(error))
 
 
     def onrecognize(self, eventname):
-        """Decorator for rules. calls the associated functions when the rule is invoked via voice"""
+        """Decorator for rules. Calls the associated functions when the rule is invoked via voice"""
         def eventdecorator(func):
-            self._func_registry[eventname].append(func)
+            """Event decorator closure thing"""
+            self._func_registry[eventname.lower()].append(func)
             return func
         return eventdecorator
 
 
-    def listen(self):
+    def listen_in_background(self):
         """Returns a callable that stops the listener"""
         stopper = self._recognizer.listen_in_background(self._source, self._callback)
         return stopper
